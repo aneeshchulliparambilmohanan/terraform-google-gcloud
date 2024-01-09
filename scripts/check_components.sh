e1cb429662086072b48b212a16b373fa2e44bf99
ANEESH CHULLIPARAMBIL MOHANAN <T998661@corp.ads>
2024-01-09 11:41:29 -0500

    trying alternative

--- scripts/check_components.sh
+++ scripts/check_components.sh
@@ -58,6 +58,7 @@ done
 if [[ ${FINAL_COMPONENT_LIST[*]} ]]; then
     echo "Installing components ${FINAL_COMPONENT_LIST[*]}";
     #$GCLOUD_PATH components install "${FINAL_COMPONENT_LIST[@]}" --quiet
+    whereis sudo
     sudo apt-get install kubectl google-cloud-sdk-kpt
 else
     echo "All components ${PROPOSED_COMPONENTS_TO_INSTALL[*]} already installed."
